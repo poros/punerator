633f0484e0392a4d3fe0a4740675cdd0085baea2
Antonio Verardi <antonio@yelp.com>
2015-11-12 18:52:10 -0800

    Various fixes

--- js/search.js
+++ js/search.js
@@ -4,6 +4,7 @@ google.setOnLoadCallback(OnLoad);
 var RESULTS_NUM = 3;
 var images;
 var words;
+var called;
 
 function searchComplete(imageSearch, pos) {
 
@@ -13,7 +14,8 @@ function searchComplete(imageSearch, pos) {
           results_urls.push(result.url)
       });
       images[pos] = results_urls
-      if (images.length == words.length) {
+      called = called + 1
+      if (called == words.length) {
           displayImages();
       }
   }
@@ -26,6 +28,7 @@ function displayImages() {
         var row = document.createElement('div');
         for (var j = 0; j < words.length; j++) {
             var imgContainer = document.createElement('span');
+            imgContainer.className = "ImageContainer"
             var newImg = document.createElement('img');
             newImg.className = "ImageResult"
             newImg.src = images[j][i];
@@ -39,11 +42,15 @@ function displayImages() {
 function search() {
     images = []
     words = document.getElementById("search_bar").value.split(" ");
+    called = 0
     for (var i = 0; i < words.length; i++) {
         var word = words[i]
         var imageSearch = new google.search.ImageSearch();
         imageSearch.setResultSetSize(RESULTS_NUM);
         imageSearch.setSearchCompleteCallback(this, searchComplete, [imageSearch, i]);
+        imageSearch.setRestriction(
+            google.search.ImageSearch.RESTRICT_IMAGESIZE,
+            google.search.ImageSearch.IMAGESIZE_MEDIUM);
         imageSearch.execute(word);
     }
 }
